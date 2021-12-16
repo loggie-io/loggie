3805f1871bacf28d44de93bab29ffb1917ef224f
OliverWoo <oliver.wu02@gmail.com>
2021-12-16 16:32:41 +0800

    (Tools) delete vendor, use mod instead
    vendor might not contain complete packages
    
    for example, github.com/segmentio/kafka-go

--- vendor/github.com/smartystreets-prototypes/go-disruptor/default_wait_strategy.go
+++
@@ -1,9 +0,0 @@
-package disruptor
-
-import "time"
-
-type DefaultWaitStrategy struct{}
-
-func NewWaitStrategy() DefaultWaitStrategy        { return DefaultWaitStrategy{} }
-func (this DefaultWaitStrategy) Gate(count int64) { time.Sleep(time.Nanosecond) }
-func (this DefaultWaitStrategy) Idle(count int64) { time.Sleep(time.Microsecond * 50) }
