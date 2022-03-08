1df7efd2c4b5f59cfbbb7ed381208391e6771f41
ethfoo <ethfoo@163.com>
2022-03-08 19:13:31 +0800

    Feat: remove govet in .golangci.yml for unsafe package usage

--- pkg/source/prometheus_exporter/config.go
+++ pkg/source/prometheus_exporter/config.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Config struct {
-	Endpoints  []string      `yaml:"endpoints,omitempty" validate:"required"`
-	Interval   time.Duration `yaml:"interval,omitempty" default:"30s"`
-	Timeout    time.Duration `yaml:"timeout,omitempty" default:"5s"`
-	ToJson     bool          `yaml:"toJson,omitempty"`
+	Endpoints []string      `yaml:"endpoints,omitempty" validate:"required"`
+	Interval  time.Duration `yaml:"interval,omitempty" default:"30s"`
+	Timeout   time.Duration `yaml:"timeout,omitempty" default:"5s"`
+	ToJson    bool          `yaml:"toJson,omitempty"`
 }
 
 func (c *Config) Validate() error {
