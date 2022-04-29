783bc956aed80a5048157af1401c9a9b6fa35455
Rongjun Wu <rongjun0821@gmail.com>
2022-04-29 13:37:46 +0800

    Fix: merge default interceptors defined in code to default interceptors defined in config (#174)

--- pkg/eventbus/listener/sink/listener.go
+++ pkg/eventbus/listener/sink/listener.go
@@ -176,24 +176,12 @@ func (l *Listener) consumer(e eventbus.SinkMetricData) {
 
 	d, ok := l.data[key]
 	if !ok {
-		data := &data{
+		l.data[key] = &data{
 			PipelineName: e.PipelineName,
 			SourceName:   e.SourceName,
 		}
-		if e.FailEventCount != 0 {
-			data.FailEventCount = int64(e.FailEventCount)
-		}
-		if e.SuccessEventCount != 0 {
-			data.SuccessEventCount = int64(e.SuccessEventCount)
-		}
-		l.data[key] = data
-		return
 	}
 
-	if e.FailEventCount != 0 {
-		d.FailEventCount += int64(e.FailEventCount)
-	}
-	if e.SuccessEventCount != 0 {
-		d.SuccessEventCount += int64(e.SuccessEventCount)
-	}
+	d.SuccessEventCount += int64(e.SuccessEventCount)
+	d.FailEventCount += int64(e.FailEventCount)
 }
