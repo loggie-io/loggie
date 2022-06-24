32be882e6d929d8a99a71d5db20006a246f7608a
zy张研 <yzhang55@trip.com>
2022-06-24 10:39:53 +0800

    use *bool

--- pkg/interceptor/normalize/grok_test.go
+++ pkg/interceptor/normalize/grok_test.go
@@ -226,55 +226,55 @@ func TestGrokProcessor_Process(t *testing.T) {
 		//		"uuid":     "54ce5d87-b94c-c40a-74a7-9cd375289334",
 		//	},
 		//},
-		{
-			/*  Patterns.txt:
-			USERNAME  [a-zA-Z0-9._-]+
-			USER      %{USERNAME}
-			INT       (?:[+-]?(?:[0-9]+))
-			WORD      \b\w+\b
-			UUID      [A-Fa-f0-9]{8}-(?:[A-Fa-f0-9]{4}-){3}[A-Fa-f0-9]{12}
-			IPV4      (?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
-			PATH      (?:%{UNIXPATH}|%{WINPATH})
-			UNIXPATH  (/[\w_%!$@:.,-]?/?)(\S+)?
-			WINPATH   ([A-Za-z]:|\\)(?:\\[^\\?*]*)+
-			MONTHNUM  (?:0?[1-9]|1[0-2])
-			MONTHDAY  (?:(?:0[1-9])|(?:[12][0-9])|(?:3[01])|[1-9])
-			YEAR      (\d\d){1,2}
-			DATE_US   %{MONTHNUM}[/-]%{MONTHDAY}[/-]%{YEAR}
-			DATE_EU   %{MONTHDAY}[./-]%{MONTHNUM}[./-]%{YEAR}
-			DATE_CN   %{YEAR}[./-]%{MONTHNUM}[./-]%{MONTHDAY}
-			DATE      %{DATE_US}|%{DATE_EU}|%{DATE_CN}
-			HOUR      (?:2[0123]|[01]?[0-9])
-			MINUTE    (?:[0-5][0-9])
-			SECOND    (?:(?:[0-5][0-9]|60)(?:[:.,][0-9]+)?)
-			TIME      ([^0-9]?)%{HOUR}:%{MINUTE}(?::%{SECOND})([^0-9]?)
-			DATESTAMP %{DATE}[- ]%{TIME}
-			*/
-			name: "use Pattern from local path",
-			fields: fields{config: &GrokConfig{
-				Match: []string{
-					"^%{DATESTAMP:datetime} (?P<file>[a-zA-Z0-9._-]+):%{INT:line}: %{IPV4:ip} %{PATH:path} %{UUID:uuid}(?P<space>[a-zA-Z]?)",
-				},
-				Target:       "body",
-				IgnoreBlank:  &T,
-				Overwrite:    &T,
-				PatternPaths: []string{"./Patterns.txt"},
-			}},
-			args: args{e: &event.DefaultEvent{
-				H: map[string]interface{}{
-					"file": "test.go",
-				},
-				B: []byte("2022/05/28 01:32:01 logTest.go:66: 192.168.0.1 /var/log/test.log 54ce5d87-b94c-c40a-74a7-9cd375289334"),
-			}},
-			want: map[string]interface{}{
-				"datetime": "2022/05/28 01:32:01", // this url just support DATE_US and DATE_EU
-				"file":     "logTest.go",
-				"line":     "66",
-				"ip":       "192.168.0.1",
-				"path":     "/var/log/test.log",
-				"uuid":     "54ce5d87-b94c-c40a-74a7-9cd375289334",
-			},
-		},
+		//{
+		//	/*  Patterns.txt:
+		//	USERNAME  [a-zA-Z0-9._-]+
+		//	USER      %{USERNAME}
+		//	INT       (?:[+-]?(?:[0-9]+))
+		//	WORD      \b\w+\b
+		//	UUID      [A-Fa-f0-9]{8}-(?:[A-Fa-f0-9]{4}-){3}[A-Fa-f0-9]{12}
+		//	IPV4      (?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
+		//	PATH      (?:%{UNIXPATH}|%{WINPATH})
+		//	UNIXPATH  (/[\w_%!$@:.,-]?/?)(\S+)?
+		//	WINPATH   ([A-Za-z]:|\\)(?:\\[^\\?*]*)+
+		//	MONTHNUM  (?:0?[1-9]|1[0-2])
+		//	MONTHDAY  (?:(?:0[1-9])|(?:[12][0-9])|(?:3[01])|[1-9])
+		//	YEAR      (\d\d){1,2}
+		//	DATE_US   %{MONTHNUM}[/-]%{MONTHDAY}[/-]%{YEAR}
+		//	DATE_EU   %{MONTHDAY}[./-]%{MONTHNUM}[./-]%{YEAR}
+		//	DATE_CN   %{YEAR}[./-]%{MONTHNUM}[./-]%{MONTHDAY}
+		//	DATE      %{DATE_US}|%{DATE_EU}|%{DATE_CN}
+		//	HOUR      (?:2[0123]|[01]?[0-9])
+		//	MINUTE    (?:[0-5][0-9])
+		//	SECOND    (?:(?:[0-5][0-9]|60)(?:[:.,][0-9]+)?)
+		//	TIME      ([^0-9]?)%{HOUR}:%{MINUTE}(?::%{SECOND})([^0-9]?)
+		//	DATESTAMP %{DATE}[- ]%{TIME}
+		//	*/
+		//	name: "use Pattern from local path",
+		//	fields: fields{config: &GrokConfig{
+		//		Match: []string{
+		//			"^%{DATESTAMP:datetime} (?P<file>[a-zA-Z0-9._-]+):%{INT:line}: %{IPV4:ip} %{PATH:path} %{UUID:uuid}(?P<space>[a-zA-Z]?)",
+		//		},
+		//		Target:       "body",
+		//		IgnoreBlank:  &T,
+		//		Overwrite:    &T,
+		//		PatternPaths: []string{"./Patterns.txt"},
+		//	}},
+		//	args: args{e: &event.DefaultEvent{
+		//		H: map[string]interface{}{
+		//			"file": "test.go",
+		//		},
+		//		B: []byte("2022/05/28 01:32:01 logTest.go:66: 192.168.0.1 /var/log/test.log 54ce5d87-b94c-c40a-74a7-9cd375289334"),
+		//	}},
+		//	want: map[string]interface{}{
+		//		"datetime": "2022/05/28 01:32:01", // this url just support DATE_US and DATE_EU
+		//		"file":     "logTest.go",
+		//		"line":     "66",
+		//		"ip":       "192.168.0.1",
+		//		"path":     "/var/log/test.log",
+		//		"uuid":     "54ce5d87-b94c-c40a-74a7-9cd375289334",
+		//	},
+		//},
 	}
 
 	log.InitDefaultLogger()
