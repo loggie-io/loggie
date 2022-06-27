2a683278dee5ec6ae9b1ef0cc63325733b790213
Alex <1398141580@qq.com>
2022-06-27 19:36:33 +0800

    Update config.go

--- pkg/sink/pulsar/config.go
+++ pkg/sink/pulsar/config.go
@@ -14,45 +14,43 @@ type pulsarConfig struct {
 	URL   string `yaml:"url,omitempty" validate:"required"`
 	Topic string `yaml:"topic,omitempty" validate:"required"`
 	// Set the operation timeout. Producer-create, subscribe and unsubscribe operations will be retried until this interval, after which the operation will be marked as failed
-	OperationTimeoutSeconds    time.Duration `yaml:"operation_timeout_seconds,omitempty"`
+	OperationTimeoutSeconds    time.Duration `yaml:"operation_timeout_seconds,omitempty" default:"30s" validate:"gt=0"`
 	UseTLS                     bool          `yaml:"useTLS,omitempty"`
 	TLSTrustCertsFilePath      string        `yaml:"tlsTrustCertsFilePath,omitempty"`
 	TLSAllowInsecureConnection bool          `yaml:"tlsAllowInsecureConnection,omitempty"`
 	// Max number of connections to a single broker that is kept in the pool. default 1
 	MaxConnectionsPerBroker int    `yaml:"maxConnectionPerBroker,omitempty"`
-	LogLevel                string `yaml:"logLevel,omitempty"`
+	LogLevel                string `yaml:"logLevel,omitempty" default:"info" validate:"oneof=info debug error"`
 
 	CertificatePath string `yaml:"certificatePath,omitempty"`
 	PrivateKeyPath  string `yaml:"privateKeyPath,omitempty"`
 	Token           string `yaml:"token,omitempty"`
 	TokenFilePath   string `yaml:"tokenFilePath,omitempty"`
 	// Timeout for the establishment of a TCP connection
-	ConnectionTimeout time.Duration `yaml:"connectionTimeout,omitempty"`
+	ConnectionTimeout time.Duration `yaml:"connectionTimeout,omitempty" default:"5s" validate:"gt=0"`
 
 	// producer name
 	Name       string            `yaml:"name,omitempty"`
 	Properties map[string]string `yaml:"properties,omitempty"`
 	// SendTimeout set the timeout for a message that is not acknowledged by the server 30s.default 30s
-	SendTimeout time.Duration `yaml:"sendTimeout,omitempty"`
+	SendTimeout time.Duration `yaml:"sendTimeout,omitempty" default:"30s" validate:"gte=-1"`
 	// MaxPendingMessages set the max size of the queue holding the messages pending to receive an acknowledgment from the broker.
-	MaxPendingMessages      int                    `yaml:"maxPendingMessages,omitempty"`
-	HashingScheme           pulsar.HashingScheme   `yaml:"hashingSchema,omitempty"`
-	CompressionType         pulsar.CompressionType `yaml:"compressionType,omitempty"`
-	Batching                bool                   `yaml:"batching,omitempty"`
-	BatchingMaxPublishDelay time.Duration          `yaml:"batchingMaxPublishDelay,omitempty"`
-	BatchingMaxMessages     uint                   `yaml:"batchingMaxMessages,omitempty"`
-	BatchingMaxSize         uint                   `yaml:"batchingMaxSize,omitempty"`
+	MaxPendingMessages int                  `yaml:"maxPendingMessages,omitempty" default:"2048" validate:"gt=0"`
+	HashingScheme      pulsar.HashingScheme `yaml:"hashingSchema,omitempty" default:"0" validate:"oneof=0 1"`
+	// Pulsar message compression type 0:noCompression,1:LZ4,2:zlib,3:zSTD
+	CompressionType pulsar.CompressionType `yaml:"compressionType,omitempty" default:"0" validate:"oneof=0 1 2 3"`
+	// 0:Default 1:Faster 2:Better
+	CompressionLevel        pulsar.CompressionLevel `yaml:"compressionLevel,omitempty" default:"0" validate:"oneof=0 1 2"`
+	BatchingMaxPublishDelay time.Duration           `yaml:"batchingMaxPublishDelay,omitempty" default:"10ms" validate:"gt=0"`
+	BatchingMaxMessages     uint                    `yaml:"batchingMaxMessages,omitempty" default:"1000" validate:"gt=0"`
+	// BatchingMaxSize specifies the maximum number of bytes permitted in a batch. (default 2048 KB)
+	// If set to a value greater than 1, messages will be queued until this threshold is reached or
+	// BatchingMaxMessages (see above) has been reached or the batch interval has elapsed.
+	BatchingMaxSize uint `yaml:"batchingMaxSize,omitempty" default:"2048" validate:"gt=0"`
 }
 
 func (c *pulsarConfig) Validate() error {
-	if len(c.URL) == 0 {
-		return errors.New("no URL configured")
-	}
-	if len(c.Topic) == 0 {
-		return errors.New("no topic configured")
-	}
 	if c.UseTLS {
-
 		if len(c.TLSTrustCertsFilePath) == 0 {
 			return errors.New("no tls_trust_certs_file_path configured")
 		}
@@ -62,27 +60,16 @@ func (c *pulsarConfig) Validate() error {
 			}
 		}
 	}
-	if len(c.LogLevel) > 0 {
-		_, err := logrus.ParseLevel(c.LogLevel)
-		if err != nil {
-			return err
-		}
-	}
-	if c.CompressionType < 0 {
-		return errors.New("compression_type is incorrect")
-	}
 	return nil
 }
 
 func getOptions(
 	config *pulsarConfig,
 ) (*pulsar.ClientOptions, *pulsar.ProducerOptions, error) {
-	err := config.Validate()
-	if err != nil {
-		return nil, nil, err
-	}
 	clientOptions := pulsar.ClientOptions{
-		URL: config.URL,
+		URL:               config.URL,
+		OperationTimeout:  config.OperationTimeoutSeconds,
+		ConnectionTimeout: config.ConnectionTimeout,
 	}
 	if config.UseTLS {
 		clientOptions.TLSTrustCertsFilePath = config.TLSTrustCertsFilePath
@@ -91,7 +78,7 @@ func getOptions(
 		}
 	}
 	if len(config.Token) > 0 {
-		clientOptions.Authentication = pulsar.NewAuthenticationToken(string(config.Token))
+		clientOptions.Authentication = pulsar.NewAuthenticationToken(config.Token)
 	}
 	if len(config.TokenFilePath) > 0 {
 		clientOptions.Authentication = pulsar.NewAuthenticationTokenFromFile(config.TokenFilePath)
@@ -105,18 +92,17 @@ func getOptions(
 		clientOptions.Logger = loggers
 	}
 
-	if config.OperationTimeoutSeconds > 0 {
-		clientOptions.OperationTimeout = config.OperationTimeoutSeconds
-	}
-	if config.ConnectionTimeout > 0 {
-		clientOptions.ConnectionTimeout = config.ConnectionTimeout
-	}
-
 	if config.TLSAllowInsecureConnection {
 		clientOptions.TLSAllowInsecureConnection = config.TLSAllowInsecureConnection
 	}
 	producerOptions := pulsar.ProducerOptions{
-		Topic: config.Topic,
+		Topic:                   config.Topic,
+		MaxPendingMessages:      config.MaxPendingMessages,
+		CompressionType:         config.CompressionType,
+		HashingScheme:           config.HashingScheme,
+		BatchingMaxSize:         config.BatchingMaxSize,
+		BatchingMaxPublishDelay: config.BatchingMaxPublishDelay,
+		BatchingMaxMessages:     config.BatchingMaxMessages,
 	}
 	if len(config.Name) > 0 {
 		producerOptions.Name = config.Name
@@ -126,25 +112,6 @@ func getOptions(
 	}
 	if len(config.Properties) > 0 {
 		producerOptions.Properties = config.Properties
-	}
-	if config.MaxPendingMessages > 0 {
-		producerOptions.MaxPendingMessages = config.MaxPendingMessages
-	}
-
-	if config.HashingScheme > 0 {
-		producerOptions.HashingScheme = config.HashingScheme
-	}
-	if config.CompressionType > 0 {
-		producerOptions.CompressionType = config.CompressionType
-	}
-	if config.BatchingMaxSize > 0 {
-		producerOptions.BatchingMaxSize = config.BatchingMaxSize
-	}
-	if config.BatchingMaxPublishDelay > 0 {
-		producerOptions.BatchingMaxPublishDelay = config.BatchingMaxPublishDelay
-	}
-	if config.BatchingMaxMessages > 0 {
-		producerOptions.BatchingMaxMessages = config.BatchingMaxMessages
 	}
 	return &clientOptions, &producerOptions, nil
 }
