6cbbfff11f20a8e9b39780844a6736f21abd0254
ethfoo <ethfoo@163.com>
2022-08-23 14:08:48 +0800

    Feat: add schema interceptor

--- pkg/util/time/time_test.go
+++ pkg/util/time/time_test.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package util
+package time
 
 import (
 	"github.com/loggie-io/loggie/pkg/util/yaml"
