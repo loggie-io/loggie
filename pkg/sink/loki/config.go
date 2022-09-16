df5306fa20d3924e0497532373bbd2a16d462b43
ziyu-zhao <zhao.ziyu2@northeastern.edu>
2022-09-16 19:06:37 +0800

    feat： (#362)
    http custom headers added in request sent to loki.

--- pkg/sink/loki/config.go
+++ pkg/sink/loki/config.go
@@ -3,9 +3,10 @@ package loki
 import "time"
 
 type Config struct {
-	URL         string        `yaml:"url,omitempty" validate:"required"`
-	ContentType string        `yaml:"contentType,omitempty" default:"json" validate:"oneof=json protobuf"`
-	TenantId    string        `yaml:"tenantId,omitempty"`
-	Timeout     time.Duration `yaml:"timeout,omitempty" default:"30s"`
-	EntryLine   string        `yaml:"entryLine,omitempty"`
+	URL         string            `yaml:"url,omitempty" validate:"required"`
+	ContentType string            `yaml:"contentType,omitempty" default:"json" validate:"oneof=json protobuf"`
+	TenantId    string            `yaml:"tenantId,omitempty"`
+	Timeout     time.Duration     `yaml:"timeout,omitempty" default:"30s"`
+	EntryLine   string            `yaml:"entryLine,omitempty"`
+	Headers     map[string]string `yaml:"header,omitempty"`
 }
