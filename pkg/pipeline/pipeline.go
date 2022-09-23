6a8c5aae63e1eb4fd667369c13ac8f382bbca951
ethfoo <ethfoo@163.com>
2022-09-23 15:48:59 +0800

    Fix: check if config is nil in setDefault, related: #368

--- pkg/pipeline/pipeline.go
+++ pkg/pipeline/pipeline.go
@@ -390,9 +390,6 @@ func (p *Pipeline) validateComponent(ctx api.Context) error {
 	if err != nil {
 		return err
 	}
-	if ctx.Properties() == nil {
-		return nil
-	}
 	return cfg.UnpackFromCommonCfg(ctx.Properties(), component.Config()).Defaults().Validate().Do()
 }
 
