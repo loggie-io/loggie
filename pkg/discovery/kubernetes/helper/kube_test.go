0c1b65202b2cafb10ad9c95e6d0ad6f3bc16e38d
lei.zhangDB <lei.zhangDB@eoitek.com>
2022-09-23 16:45:32 +0800

    fix(source): Fix the problem of labelSelector

--- pkg/discovery/kubernetes/helper/kube_test.go
+++ pkg/discovery/kubernetes/helper/kube_test.go
@@ -1,7 +1,6 @@
 package helper
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -78,46 +77,68 @@ func TestLabelsSubset(t *testing.T) {
 }
 
 func TestMatchSelector(t *testing.T) {
-	ret := MatchStringMap(map[string]string{
-		"a": "b",
-	}, map[string]string{
-		"a": "b",
-	})
-	assert.Equal(t, ret, true)
+	type args struct {
+		i map[string]string
+		j map[string]string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "The second parameter is more than the first parameter",
+			args: args{
+				i: map[string]string{
+					"a": "b",
+				},
+				j: map[string]string{
+					"a": "b",
+					"c": "d",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "The first parameter is more than the second parametert",
+			args: args{
+				i: map[string]string{
+					"a": "b",
+					"c": "d",
+				},
+				j: map[string]string{
+					"a": "b",
+				},
+			},
+			want: false,
+		},
+		{
+			name: "Both parameters are nil",
+			args: args{
+				i: nil,
+				j: nil,
+			},
+			want: true,
+		},
+		{
+			name: "Both parameters are equal",
+			args: args{
+				i: map[string]string{
+					"a": "b",
+				},
+				j: map[string]string{
+					"a": "b",
+				},
+			},
+			want: true,
+		},
+	}
 
-	ret = MatchStringMap(map[string]string{
-		"a": "b",
-	}, map[string]string{
-		"a": "1",
-	})
-	assert.Equal(t, ret, false)
-
-	ret = MatchStringMap(nil, nil)
-	assert.Equal(t, ret, true)
-
-	ret = MatchStringMap(map[string]string{
-		"a": "b",
-	}, nil)
-	assert.Equal(t, ret, false)
-
-	ret = MatchStringMap(map[string]string{
-		"a": "b",
-	}, map[string]string{})
-	assert.Equal(t, ret, false)
-
-	ret = MatchStringMap(map[string]string{
-		"a": "b",
-	}, map[string]string{
-		"a": "b",
-		"c": "d",
-	})
-	assert.Equal(t, ret, false)
-
-	ret = MatchStringMap(map[string]string{
-		"a": "b",
-		"c": "d",
-	}, map[string]string{
-		"a": "b",
-	})
-	assert.Equal(t, ret, false)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MatchStringMap(tt.args.i, tt.args.j); got != tt.want {
+				t.Errorf("MatchStringMap() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
