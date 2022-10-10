90b0825d088c8ff02b46eef5389c35ff20b17291
lei.zhangDB <lei.zhangDB@eoitek.com>
2022-10-10 10:32:40 +0800

    feature(source): Add franz-go support

---
+++ pkg/sink/franz/sink_test.go
@@ -0,0 +1,41 @@
+package franz
+
+import (
+	"context"
+	"fmt"
+	"github.com/loggie-io/loggie/pkg/core/api"
+	"github.com/loggie-io/loggie/pkg/core/log"
+	"github.com/twmb/franz-go/pkg/kgo"
+	"io"
+	"testing"
+	"time"
+)
+
+func Test_SinkerWrite(t *testing.T) {
+	log.InitDefaultLogger()
+	type fields struct {
+		W             io.Writer
+		Size          int
+		FlushInterval time.Duration
+	}
+	type args struct {
+		bs []byte
+	}
+
+	var c Config
+	c.Brokers = []string{
+		"192.168.110.8:9092",
+		"192.168.110.12:9092",
+		"192.168.110.16:9092",
+	}
+
+	sinker := NewSink()
+	sinker.config = &c
+	var sinkerContext api.Context
+	sinker.Init(sinkerContext)
+	sinker.Start()
+	ctx := context.Background()
+	record := &kgo.Record{Topic: "franz-test", Value: []byte("1111111111111111111111111")}
+	ret := sinker.writer.ProduceSync(ctx, record)
+	fmt.Println(ret)
+}
