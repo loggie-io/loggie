4b63ae84d23412c0c6e6d073cbabd153e80da4f2
ziyu-zhao <zhao.ziyu2@northeastern.edu>
2022-12-15 15:48:18 +0800

    loki sink https insecureSkipVerify (#422)

--- pkg/sink/loki/loki.go
+++ pkg/sink/loki/loki.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 Loggie Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package loki
 
 import (
@@ -69,7 +85,10 @@ func (s *Sink) String() string {
 func (s *Sink) Init(context api.Context) error {
 	s.name = context.Name()
 
-	cli, err := config.NewClientFromConfig(config.DefaultHTTPClientConfig, "loggie")
+	defaultHTTPConfig := config.DefaultHTTPClientConfig
+	defaultHTTPConfig.TLSConfig.InsecureSkipVerify = s.config.InsecureSkipVerify
+
+	cli, err := config.NewClientFromConfig(defaultHTTPConfig, "loggie")
 	if err != nil {
 		log.Error("init loki default client failed: %v", err)
 		return nil
