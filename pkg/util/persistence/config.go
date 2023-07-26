6d18d0f8474d799cf1f4155da8a64908ec1cb857
ethfoo <ethfoo@163.com>
2023-07-26 19:42:58 +0800

    Release: add v1.5.0-rc.0 changelog (#597)

--- pkg/util/persistence/config.go
+++ pkg/util/persistence/config.go
@@ -34,7 +34,6 @@ type DbConfig struct {
 	File                 string        `yaml:"file,omitempty"`
 	FlushTimeout         time.Duration `yaml:"flushTimeout,omitempty" default:"2s"`
 	BufferSize           int           `yaml:"bufferSize,omitempty" default:"2048"`
-	TableName            string        `yaml:"tableName,omitempty" default:"registry"`
 	CleanInactiveTimeout time.Duration `yaml:"cleanInactiveTimeout,omitempty" default:"504h"` // default records not updated in 21 days will be deleted
 	CleanScanInterval    time.Duration `yaml:"cleanScanInterval,omitempty" default:"1h"`
 }
